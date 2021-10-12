f5f16a9fb2f7149c156e65cfc28e126cd34ee838
Will Tidwell <jwtidwell1@crimson.ua.edu>
2021-10-11 23:47:10 -0500

    Pig is at 90 percent.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace pig_project
             int player2Score = 0;
             Console.WriteLine("Welcome to Pig");
             Console.WriteLine( "test1" );
-            pigPlayer1( ref player1Score );
+            pigPlayer1( ref player1Score, ref player2Score );
         }
         static int random1()
         {
@@ -19,7 +19,7 @@ namespace pig_project
             number = randomNumber.Next( 1, 10 );
             return number;
         }
-        static void pigPlayer1( ref int player1Score )
+        static void pigPlayer1( ref int player1Score, ref int  player2Score )
         {
             Console.WriteLine( "Player 1 is rolling" );
             while ( player1Score < 100 )
@@ -33,15 +33,37 @@ namespace pig_project
                  else
                  {
                      Console.WriteLine( "Your turn has ended" );
-                     player2Method();
+                     player2Method( ref player2Score, ref player1Score );
                      break;
                  }
                  
             }
         }
-        static void player2Method()
+        static void player2Method( ref int player2Score, ref int player1Score )
         {
             Console.WriteLine( "It is player 2's turn" );
+            pigPlayer2( ref player2Score, ref player1Score);
+        }
+        static void pigPlayer2( ref int player2Score, ref int player1Score)
+        {
+            Console.WriteLine( "Player 2 is rolling" );
+            while ( player2Score < 100 )
+            {
+                 int addScore = random1();
+                 if ( addScore != 1 )
+                 {
+                     player2Score = addScore + player2Score;
+                     Console.WriteLine( player2Score );
+                 }
+                 else
+                 {
+                     Console.WriteLine( "Your turn has ended" );
+                     Console.WriteLine( "It is back to player 1" );
+                     pigPlayer1( ref player1Score, ref player2Score );
+                     break;
+                 }
+                 
+            }
         }
             
     }
